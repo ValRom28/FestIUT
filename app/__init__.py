18247f47959cae1f2cf6c543f9c040180a9347fc
ValRom <valentin.romanet@etu.univ-orleans.fr>
2024-01-10 15:01:11 +0100

    fix : modif de app en __init__

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = ''
+app.config['SECRET_KEY'] = 'ab23hjlOp9'
 if not os.path.exists(os.path.normpath(os.path.join(os.path.dirname(__file__), 'database'))):
     os.makedirs(os.path.normpath(os.path.join(os.path.dirname(__file__), 'database')))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.normpath(os.path.join(os.path.dirname(__file__), 'database/app.db'))
@@ -13,9 +13,5 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-from app import views
 from app import models
 from app import database
-from app import requests
-from app import forms
-from app import nlp
