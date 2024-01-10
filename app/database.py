87680cc5d0c920c9d80a86e20253de5bcb6cc480
ValRom <valentin.romanet@etu.univ-orleans.fr>
2024-01-10 15:01:28 +0100

    feat : models terminé + avancement database

---
+++ app/database.py
@@ -0,0 +1,11 @@
+from app import app, db
+from sqlalchemy.orm import sessionmaker
+from .models import (Appartenir, Artiste, Billet, Concert, EtreStyle,
+                     EtreType, Favoris, Groupe, Hebergement, Instrument,
+                     Jouer, Lieu, OrganiserConcert, Possede, Reserver,
+                     Style, SousStyle, Spectateur, Type)
+
+@app.cli.command('initdb')
+def initdb_command():
+    db.create_all()
+    print('Initialized the database.')
