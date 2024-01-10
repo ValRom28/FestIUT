9366a0d7f9550bb4821d64823614c23041a4aacf
Zenido <thibault.saint-leger@etu.univ-orleans.fr>
2024-01-10 18:30:47 +0100

    rajout d'artiste et groupe + modifications des classés associés (APPARTENIR, JOUER et ETRESTYLE

--- app/static/sql/insertion.sql
+++ app/static/sql/insertion.sql
@@ -2,6 +2,14 @@
 INSERT INTO ARTISTE (idArtiste, nomArtiste) VALUES (1, 'DJ Snake');
 INSERT INTO ARTISTE (idArtiste, nomArtiste) VALUES (2, 'Louane');
 INSERT INTO ARTISTE (idArtiste, nomArtiste) VALUES (3, 'Liam Sottier');
+INSERT INTO ARTISTE (idArtiste, nomArtiste) VALUES (4, 'Adele');
+INSERT INTO ARTISTE (idArtiste, nomArtiste) VALUES (5, 'Ed Sheeran');
+INSERT INTO ARTISTE (idArtiste, nomArtiste) VALUES (6, 'Beyoncé');
+INSERT INTO ARTISTE (idArtiste, nomArtiste) VALUES (7, 'Taylor Swift');
+INSERT INTO ARTISTE (idArtiste, nomArtiste) VALUES (8, 'Bruno Mars');
+INSERT INTO ARTISTE (idArtiste, nomArtiste) VALUES (9, 'Alicia Keys');
+INSERT INTO ARTISTE (idArtiste, nomArtiste) VALUES (10, 'John Legend');
+
 
 -- Insertions pour la table HEBERGEMENT
 INSERT INTO HEBERGEMENT (idHebergement, nomHebergement, addresseHebergement) VALUES (1, 'Hôtel Luxe', '123 Rue Principale');
@@ -10,8 +18,13 @@ INSERT INTO HEBERGEMENT (idHebergement, nomHebergement, addresseHebergement) VAL
 
 -- Insertions pour la table GROUPE
 INSERT INTO GROUPE (idGroupe, nomGroupe, idHebergement) VALUES (1, 'Les Artistes Brillants', 1);
-INSERT INTO GROUPE (idGroupe, nomGroupe, idHebergement) VALUES (2, 'Ensemble Harmonie', 2);
-INSERT INTO GROUPE (idGroupe, nomGroupe, idHebergement) VALUES (3, 'Les Mélodies Divines', 3);
+INSERT INTO GROUPE (idGroupe, nomGroupe, idHebergement) VALUES (2, 'Thibault et ses esclaves', 2);
+INSERT INTO GROUPE (idGroupe, nomGroupe, idHebergement) VALUES (3, 'La Saint-Leger tout puissant', 3);
+INSERT INTO GROUPE (idGroupe, nomGroupe, idHebergement) VALUES (4, 'IUTO', 1);
+INSERT INTO GROUPE (idGroupe, nomGroupe, idHebergement) VALUES (5, 'SLR', 2);
+INSERT INTO GROUPE (idGroupe, nomGroupe, idHebergement) VALUES (6, 'La Z compagnie', 3);
+INSERT INTO GROUPE (idGroupe, nomGroupe, idHebergement) VALUES (7, 'Je fait toutou seul', 3);
+
 
 -- Insertions pour la table LIEU
 INSERT INTO LIEU (idLieu, nomLieu, jaugeLieu) VALUES (1, 'Stade Olympique', 50000);
@@ -48,11 +61,22 @@ INSERT INTO INSTRUMENT (idInstrument, nomInstrument) VALUES (3, 'Piano');
 INSERT INTO JOUER (idArtiste, idInstrument) VALUES (1, 1);
 INSERT INTO JOUER (idArtiste, idInstrument) VALUES (2, 2);
 INSERT INTO JOUER (idArtiste, idInstrument) VALUES (3, 3);
+INSERT INTO JOUER (idArtiste, idInstrument) VALUES (4, 3);
+INSERT INTO JOUER (idArtiste, idInstrument) VALUES (5, 1);
+INSERT INTO JOUER (idArtiste, idInstrument) VALUES (6, 2);
+INSERT INTO JOUER (idArtiste, idInstrument) VALUES (7, 1);
+INSERT INTO JOUER (idArtiste, idInstrument) VALUES (8, 2);
+INSERT INTO JOUER (idArtiste, idInstrument) VALUES (9, 3);
+INSERT INTO JOUER (idArtiste, idInstrument) VALUES (10, 1);
 
 -- Insertions pour la table ETRESTYLE
 INSERT INTO ETRESTYLE (idStyle, idGroupe) VALUES (1, 1);
 INSERT INTO ETRESTYLE (idStyle, idGroupe) VALUES (2, 2);
 INSERT INTO ETRESTYLE (idStyle, idGroupe) VALUES (3, 3);
+INSERT INTO ETRESTYLE (idStyle, idGroupe) VALUES (2, 4);
+INSERT INTO ETRESTYLE (idStyle, idGroupe) VALUES (1, 5);
+INSERT INTO ETRESTYLE (idStyle, idGroupe) VALUES (3, 6);
+INSERT INTO ETRESTYLE (idStyle, idGroupe) VALUES (2, 7);
 
 -- Insertions pour la table TYPE
 INSERT INTO TYPE (idType, nbJours, prix, ageMin, ageMax) VALUES (1, 3, 50.0, 18, 60);
@@ -78,6 +102,14 @@ INSERT INTO POSSEDE (idStyle, idSousStyle) VALUES (2, 3);
 INSERT INTO APPARTENIR (idArtiste, idGroupe) VALUES (1, 1);
 INSERT INTO APPARTENIR (idArtiste, idGroupe) VALUES (2, 2);
 INSERT INTO APPARTENIR (idArtiste, idGroupe) VALUES (3, 3);
+INSERT INTO APPARTENIR (idArtiste, idGroupe) VALUES (4, 4);
+INSERT INTO APPARTENIR (idArtiste, idGroupe) VALUES (5, 5);
+INSERT INTO APPARTENIR (idArtiste, idGroupe) VALUES (6, 6);
+INSERT INTO APPARTENIR (idArtiste, idGroupe) VALUES (7, 7);
+INSERT INTO APPARTENIR (idArtiste, idGroupe) VALUES (8, 7);
+INSERT INTO APPARTENIR (idArtiste, idGroupe) VALUES (9, 7);
+INSERT INTO APPARTENIR (idArtiste, idGroupe) VALUES (10, 7);
+
 
 -- Insertions pour la table ORGANISERCONCERT
 INSERT INTO ORGANISERCONCERT (idGroupe, idConcert) VALUES (1, 1);
