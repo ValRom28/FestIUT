b3a317d67f5b557a33e2d20a3751821fda380005
Arthur.villette <arthur.villette@etu.univ-orleans.fr>
2024-01-10 19:17:49 +0100

    test: test de fonctionnement

--- app/views.py
+++ app/views.py
@@ -11,18 +11,22 @@ from app.forms import *
 from app import login_manager
 import base64
 
-@app.route('/editUser', methods=['GET', 'POST'])
-@activated_required
-@login_required
-def editUser():
-    user = get_user_by_id(current_user.get_id())
-    form = EditUserForm(id=user.idPompier, nom=user.nomPompier, prenom=user.prenomPompier, mail=user.emailPompier, mdp=user.mdpPompier, photo=user.photoPompier,telephone=user.telephonePompier)
-    page_name = f'Modifier le profil : {user.nomPompier} {user.prenomPompier}'
-    if form.validate_on_submit():
-        password = generate_password_hash(form.mdp.data).decode('utf-8')
-        if form.photo.data:
-            encoded_photo = base64.b64encode(form.photo.data.read())
-            update_user_photo(user.idPompier, encoded_photo)
-        update_user(user.idPompier, form.prenom.data, form.nom.data, form.mail.data, form.telephone.data, password)
-        return redirect(url_for('user'))
-    return render_template('editUser.html', nom_page=page_name, user=user, form=form, notification_enabled=user_has_notifications(current_user.get_id()), is_admin =get_user_by_id(current_user.get_id()).idRole == 1)
+# @app.route('/editUser', methods=['GET', 'POST'])
+# @activated_required
+# @login_required
+# def editUser():
+#     user = get_user_by_id(current_user.get_id())
+#     form = EditUserForm(id=user.idPompier, nom=user.nomPompier, prenom=user.prenomPompier, mail=user.emailPompier, mdp=user.mdpPompier, photo=user.photoPompier,telephone=user.telephonePompier)
+#     page_name = f'Modifier le profil : {user.nomPompier} {user.prenomPompier}'
+#     if form.validate_on_submit():
+#         password = generate_password_hash(form.mdp.data).decode('utf-8')
+#         if form.photo.data:
+#             encoded_photo = base64.b64encode(form.photo.data.read())
+#             update_user_photo(user.idPompier, encoded_photo)
+#         update_user(user.idPompier, form.prenom.data, form.nom.data, form.mail.data, form.telephone.data, password)
+#         return redirect(url_for('user'))
+#     return render_template('editUser.html', nom_page=page_name, user=user, form=form, notification_enabled=user_has_notifications(current_user.get_id()), is_admin =get_user_by_id(current_user.get_id()).idRole == 1)
+
+@app.route('/login')
+def login():
+    return render_template('connexion.html')
